3bef432714090127651518fa8b258b5d1fc299e0
nganhuyenthinguyen <thinguyen@campus.tu-berlin.de>
2022-07-17 10:15:27 +0200

    formatting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,13 +17,13 @@ export default {
   methods: {
     download() {
       var text = this.text;
-      var size = this.size/0.35;
+      var size = this.size / 0.35;
       var height = text.split("\n").length * size;
       console.log(height);
       var doc = new jsPDF('l', 'mm', [this.width, height]);
       let style = this.bold ? 'bold' : '';
       style = style += this.italic ? 'italic' : '';
-      if(style == '') {
+      if (style == '') {
         style = 'normal';
       }
       console.log(style);
@@ -39,44 +39,67 @@ export default {
 </script>
 
 <template>
+  <div class="container">
+    <div class="row">
+      <textarea v-model="text" placeholder="Type here"></textarea>
+    </div>
+    <div class="row">
+      <div class="col-sm">
 
-  <textarea v-model="text" placeholder="Type here"></textarea>
-  <label for="fonts"> Font: </label>
-  <select id="fonts" name="fonts" v-model="font">
-    <option value="arial">Arial</option>
-    <option value="verdana">Verdana</option>
-    <option value="times">Times New Roman</option>
-    <option value="georgia">Georgia</option>
-  </select>
+        <label for="fonts"> Font: </label>
+      </div>
+      <div class="col-sm">
+        <select id="fonts" name="fonts" v-model="font">
+          <option value="arial">Arial</option>
+          <option value="verdana">Verdana</option>
+          <option value="times">Times New Roman</option>
+          <option value="georgia">Georgia</option>
+        </select>
+      </div>
+      <div class="col-sm">
+        <label for="fonts"> Size: </label>
+      </div>
+      <div class="col-sm">
+        <input v-model="size" type="number" style="width: 5em">
+      </div>
+      <div class="col-1">
+        <input type="checkbox" id="bold" v-model="bold">
+        <label for="bold"> b</label><br>
+      </div>
+      <div class="col-1">
+        <input type="checkbox" id="italic" v-model="italic">
+        <label for="italic"> i</label><br>
+      </div>
+      <div class="col-1">
+        <input type="checkbox" id="underlined" v-model="underlined">
+        <label for="underlined"> u</label><br>
+      </div>
+      <div class="col-sm">
 
-  <label for="fonts"> Size: </label>
-  <input v-model="size" type="number" style="width: 5em">
+        <button type="button" @click="download">download</button>
+      </div>
+    </div>
 
-  <input type="checkbox" id="bold" v-model="bold">
-  <label for="bold"> b</label><br>
-  <input type="checkbox" id="italic" v-model="italic">
-  <label for="italic"> i</label><br>
-  <input type="checkbox" id="underlined" v-model="underlined">
-  <label for="underlined"> u</label><br>
+    <div class="row">
 
-  <button type="button" @click="download">download</button>
+      <div class="page" v-bind:style="{ 'width': width + 'mm' }">
+        <br>
 
-  <div class="page" v-bind:style="{ 'width': width + 'mm' }">
-    <br>
+        <p class="print" v-bind:style="{
+          'font-weight': (bold ? 'bold' : 'normal'),
+          'text-decoration': (underlined ? 'underline' : 'none'),
+          'font-style': (italic ? 'italic' : 'normal'),
+          'font-family': font,
+          'font-size': size + 'mm'
+        }">
+          {{ text }}
+        </p>
 
-    <p class="print" v-bind:style="{
-      'font-weight': (bold ? 'bold' : 'normal'),
-      'text-decoration': (underlined ? 'underline' : 'none'),
-      'font-style': (italic ? 'italic' : 'normal'),
-      'font-family': font,
-      'font-size': size + 'mm'
-    }">
-      {{ text }}
-    </p>
+        <br>
+      </div>
 
-    <br>
+    </div>
   </div>
-
 </template>
 
 <style>
