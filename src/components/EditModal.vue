f12986380f1637dbfdf0420e5244921e825f7e73
nganhuyenthinguyen <thinguyen@campus.tu-berlin.de>
2022-07-24 12:41:22 +0200

    fixes

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -66,16 +66,16 @@ export default {
 <template>
     <!-- Button trigger modal -->
     <button type="button" class="btn btn-secondary btn-lg ml-1" @click="reset" data-bs-toggle="modal"
-        data-bs-target="#exampleModal">
+        data-bs-target="#editModal">
         Edit
     </button>
 
     <!-- Modal -->
-    <div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-fullscreen-sm-down modal-dialog-scrollable modal-lg modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Edit Ribbon</h5>
+                    <h5 class="modal-title" id="editModalLabel">Edit Ribbon</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
--- src/components/HelpModal.vue
+++ src/components/HelpModal.vue
@@ -7,23 +7,28 @@ export default {
 
 <template>
     <!-- Button trigger modal -->
-    <a class="nav-link" aria-current="page" data-bs-toggle="modal"
-        data-bs-target="#exampleModal">About</a>
+    <a class="nav-link" aria-current="page" href="" data-bs-toggle="modal" data-bs-target="#helpModal">Help</a>
     <!-- Modal -->
-    <div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-fullscreen-sm-down modal-dialog-scrollable modal-lg modal-dialog-centered">
+    <div class="modal" id="helpModal" tabindex="-1" aria-labelledby="helpModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen-sm-down modal-dialog-scrollable modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Edit Ribbon</h5>
+                    <h5 class="modal-title" id="helpModalLabel">Help</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-<p>Meow</p>
+                    <p>
+                        Click inside the ribbon to edit.
+                        Change font, styles and size in top bar.
+                        Click rotate to rotate text by 90°.
+                        Choose preset text.
+                        Click "Edit" for Advanced settings.
+                        In advanced settings change ribbon width or add a motif.
+                        Change motif's size and position as well as margin to text.
+                    </p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal"
-                        @click="$emit('madechanges', this.settings)">Save changes</button>
                 </div>
             </div>
         </div>
